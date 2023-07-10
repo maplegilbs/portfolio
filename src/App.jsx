943d3e33320d08f31640c59794546ed29d75db35
Scott Gilbert <scottvtgilbert@gmail.com>
2023-07-09 20:54:49 -0400

    Troubleshooting deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
 import { PrimaryHeader } from './components/PrimaryHeader';
 import { PrimaryFooter } from './components/PrimaryFooter';
 
@@ -8,7 +8,8 @@ function App() {
     <div className="App">
       <div className='page--wrapper'>
         <PrimaryHeader />
-        <Outlet />
+        {/* <Outlet /> */}
+        <>Testing.....</>
         <PrimaryFooter />
       </div>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -2,48 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { About } from './pages/About.jsx';
-import { Projects } from './pages/Projects.jsx';
-import { Work } from './pages/Work.jsx';
-import { Personal } from './pages/Personal.jsx';
-import { Contact } from './pages/Contact.jsx';
+// import { About } from './pages/About.jsx';
+// import { Projects } from './pages/Projects.jsx';
+// import { Work } from './pages/Work.jsx';
+// import { Personal } from './pages/Personal.jsx';
+// import { Contact } from './pages/Contact.jsx';
 
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    // errorElement: <About />,
-    children: [
-      { 
-        index: true,
-        element: <About />,
-      },
-      {
-        path: 'projects',
-        element: <Projects />,
-      },
-      {
-        path: 'work',
-        element: <Work />
-      },
-      {
-        path: 'personal',
-        element: <Personal />
-      },
-      {
-        path: 'contact',
-        element: <Contact />
-      },
-    ]
-  },
-  // Add more page routes below
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     // errorElement: <About />,
+//     children: [
+//       { 
+//         index: true,
+//         element: <About />,
+//       },
+//       {
+//         path: 'projects',
+//         element: <Projects />,
+//       },
+//       {
+//         path: 'work',
+//         element: <Work />
+//       },
+//       {
+//         path: 'personal',
+//         element: <Personal />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       },
+//     ]
+//   },
+//   // Add more page routes below
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App />
   </React.StrictMode>
 );
