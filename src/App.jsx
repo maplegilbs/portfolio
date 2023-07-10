4a5d6598985b028ef4353d533a92be9e306aea78
Scott Gilbert <scottvtgilbert@gmail.com>
2023-07-09 20:55:55 -0400

    Troubleshooting deployment2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css';
 // import { Outlet } from 'react-router-dom';
-import { PrimaryHeader } from './components/PrimaryHeader';
-import { PrimaryFooter } from './components/PrimaryFooter';
+// import { PrimaryHeader } from './components/PrimaryHeader';
+// import { PrimaryFooter } from './components/PrimaryFooter';
 
 function App() {
   return (
     <div className="App">
       <div className='page--wrapper'>
-        <PrimaryHeader />
+        {/* <PrimaryHeader /> */}
         {/* <Outlet /> */}
         <>Testing.....</>
-        <PrimaryFooter />
+        {/* <PrimaryFooter /> */}
       </div>
     </div>
   );
