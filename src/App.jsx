05226dfe9b0c32191ed452042d57de7d3f7223b5
Scott Gilbert <scottvtgilbert@gmail.com>
2023-07-09 22:03:38 -0400

    Fixed initial issue of homepage type in package.json, redeploy with front end routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
-// import { Outlet } from 'react-router-dom';
-// import { PrimaryHeader } from './components/PrimaryHeader';
-// import { PrimaryFooter } from './components/PrimaryFooter';
+import { Outlet } from 'react-router-dom';
+import { PrimaryHeader } from './components/PrimaryHeader';
+import { PrimaryFooter } from './components/PrimaryFooter';
 
 function App() {
   return (
     <div className="App">
       <div className='page--wrapper'>
-        {/* <PrimaryHeader />
+        <PrimaryHeader />
         <Outlet />
-        <PrimaryFooter /> */}
+        <PrimaryFooter />
         <p>Testing 1234</p>
       </div>
     </div>
