214764bfe6ac419821dad655323f1f12beab2b40
Scott Gilbert <scottvtgilbert@gmail.com>
2023-07-09 20:59:49 -0400

    Undoing browser router removal test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
-// import { Outlet } from 'react-router-dom';
-// import { PrimaryHeader } from './components/PrimaryHeader';
-// import { PrimaryFooter } from './components/PrimaryFooter';
+import { Outlet } from 'react-router-dom';
+import { PrimaryHeader } from './components/PrimaryHeader';
+import { PrimaryFooter } from './components/PrimaryFooter';
 
 function App() {
   return (
     <div className="App">
       <div className='page--wrapper'>
-        {/* <PrimaryHeader /> */}
-        {/* <Outlet /> */}
-        <>Testing.....</>
-        {/* <PrimaryFooter /> */}
+        <PrimaryHeader />
+        <Outlet />
+        <PrimaryFooter />
       </div>
     </div>
   );
