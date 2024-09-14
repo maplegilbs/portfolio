0830898ead75b8fe41d6050ba12474d0d9a7d224
Scott Gilbert <scottvtgilbert@gmail.com>
2024-09-14 17:24:08 -0400

    Updating styles on projects component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -22,18 +22,10 @@ function VideoModal({ selectedVideo, setSelectedVideo }) {
 }
 
 export function Project({ project, isMobile, isEven, windowY }) {
-    const [isInFullView, setIsInFullView] = useState(false);
     const [selectedVideo, setSelectedVideo] = useState(null)
     const imageRef = useRef(null);
     const [isHovered, setIsHovered] = useState(false)
     let techStack = project.technologies.map((techItem, index) => <li key={index}>{techItem}</li>)
-
-    useEffect(() => {
-        if (imageRef) {
-            let imageLoc = imageRef.current.getBoundingClientRect();
-            (imageLoc.top > 0 && imageLoc.bottom < window.innerHeight) ? setIsInFullView(true) : setIsInFullView(false)
-        }
-    }, [windowY])
 
     return (
         <section className={`project-section ${!isEven ? 'image-left' : 'image-right'}`}>
@@ -46,11 +38,11 @@ export function Project({ project, isMobile, isEven, windowY }) {
                 <br />
             </div>
 
-            <div ref={imageRef} className={`${(isHovered || isInFullView) ? 'project-image project-image--hovered' : 'project-image'}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} >
+            <div ref={imageRef} className='project-image'>
                 <img src={`assets/${project.imgFileName}`} alt={project.imgAlt} />
                 {project.mobileImgFileName && <img className='mobile-image-prvw' src={`assets/${project.mobileImgFileName}`} />}
                 <a target='_blank' href={project.url}>
-                    <button className={`project-image__button ${(isHovered || isInFullView) ? 'project-image__button--hovered' : ''}`}>View Live Site</button>
+                    <button className='project-image__button'>View Live Site</button>
                 </a>
             </div>
 
